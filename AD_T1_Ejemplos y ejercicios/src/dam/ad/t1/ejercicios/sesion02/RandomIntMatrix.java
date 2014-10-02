7b15ad09f7a9c8ebf2b5e3d5d5f74d4291fc4bc1
lmlopez <lmlopez@salesianos-triana.com>
2014-10-02 08:22:07 +0200

    RandomIntMatrix

---
+++ AD_T1_Ejemplos y ejercicios/src/dam/ad/t1/ejercicios/sesion02/RandomIntMatrix.java
@@ -0,0 +1,59 @@
+package dam.ad.t1.ejercicios.sesion02;
+
+import java.util.Random;
+
+/*
+ * Clase de utilidades con métodos para crear e imprimir matrices bidimensionales enteras con datos aleatorios
+ * 
+ * Salesianos Triana
+ * Desarrollo de Aplicaciones Multiplataforma
+ * Acceso a Datos
+ * Tema 1. Sesión 2.
+ * 
+ * @author Luis Miguel López Magaña
+ * 
+ */
+public class RandomIntMatrix {
+
+	
+	
+	
+	/*
+	 * Este método recibe el tamaño de la matriz a construir, y devuelve
+	 * una matriz bidimensional de datos aleatorios, con el tamaño
+	 * proporcionado.
+	 * 
+	 * @param filas			Número de filas de la matriz
+	 * @param columnas		Número de columnas de la matriz.
+	 * @return				Matriz <code>int[][]</code> de datos aleatorios
+	 */
+	public static int[][] randomIntMatrix(int filas, int columnas) {
+		int[][] m = new int[filas][columnas];
+
+		Random r = new Random();
+
+		for (int i = 0; i < filas; i++)
+			for (int j = 0; j < columnas; j++)
+				m[i][j] = r.nextInt();
+
+		return m;
+	}
+
+	/*
+	 * Este método recibe como argumento una matriz entera bidimensinonal
+	 * y la imprime convenientemente por consola.
+	 * 
+	 * @param m				Matriz a imprimir
+	 */
+	public static void printIntMatrix(int[][] m) {
+		int filas = m.length;
+		int columnas = m[0].length;
+		System.out.println("Filas: " + filas + " - Columnas: " + columnas);
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[0].length; j++)
+				System.out.print(m[i][j] + "\t");
+			System.out.println();
+		}
+	}
+
+}
